e01d16190a4ad527700633427bd64a6e711106e1
Gabriel Carneiro Jr <gacarneirojr@hotmail.com>
2025-06-13 16:23:46 -0300

    feat: Add validation for base price in Property constructor

--- src/domain/entities/property.ts
+++ src/domain/entities/property.ts
@@ -16,6 +16,9 @@ export class Property {
     if (maxGuests <= 0) {
       throw new Error("O número máximo de hóspedes deve ser maior que zero");
     }
+    if (basePricePerNight <= 0) {
+      throw new Error("O preço base deve ser maior que zero");
+    }
     this.id = id;
     this.name = name;
     this.description = description;
