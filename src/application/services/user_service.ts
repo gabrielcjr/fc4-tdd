4b398527190721a15f9f078a47bab536605f1b97
Gabriel Carneiro Jr <gacarneirojr@hotmail.com>
2025-06-13 16:23:28 -0300

    feat: Implement createUser method in UserService for user creation

--- src/application/services/user_service.ts
+++ src/application/services/user_service.ts
@@ -1,9 +1,14 @@
 import { User } from "../../domain/entities/user";
 import { UserRepository } from "../../domain/repositories/user_repository";
+import { CreateUserDTO } from "../dtos/create_user_dto";
 
 export class UserService {
   constructor(private readonly userRepository: UserRepository) {}
   async findUserById(id: string): Promise<User | null> {
     return this.userRepository.findById(id);
+  }
+  async createUser(dto: CreateUserDTO): Promise<void> {
+    const user = new User(dto.id, dto.name);
+    this.userRepository.save(user);
   }
 }
